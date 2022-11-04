aabb32e88511bfe3447a4b410831668ca18e9619
Zeno Lösch <54742442+zeloe@users.noreply.github.com>
2022-11-04 17:07:07 +0100

    added parameter saving

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -266,15 +266,17 @@ juce::AudioProcessorEditor* ReverbZenAudioProcessor::createEditor()
 //==============================================================================
 void ReverbZenAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
+    juce::MemoryOutputStream mos (destData, true);
+                     treeState.state.writeToStream(mos);
 }
 
 void ReverbZenAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
+    auto tree = juce::ValueTree::readFromData(data, sizeInBytes);
+                          if(tree.isValid() )
+                          {
+                              treeState.replaceState(tree);
+                          }
 }
 
 //==============================================================================
