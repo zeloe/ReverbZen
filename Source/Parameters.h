967fd94b31becda9c86abfab2edd2d9827d95686
Zeno Lösch <54742442+zeloe@users.noreply.github.com>
2022-11-11 13:38:03 +0100

    added parameters as const strings

---
+++ Source/Parameters.h
@@ -0,0 +1,23 @@
+/*
+  ==============================================================================
+
+    Parameters.h
+    Created: 11 Nov 2022 1:15:35pm
+    Author:  Zeno  Loesch
+
+  ==============================================================================
+*/
+
+#pragma once
+#import "JuceHeader.h"
+
+const juce::String sReverb = "Reverb";
+const juce::String sPreDelay = "PreDelay";
+const juce::String sLowpass = "Lowpass";
+const juce::String sErDelay = "ErDelay";
+const juce::String sEramp = "Eramp";
+const juce::String sMix = "Mix";
+const juce::String sDamp = "Damp";
+const juce::String sHighPassFreq = "HighPassFreq";
+const juce::String sPrePost = "Pre/Post";
+const juce::String sBypass = "Bypass";
